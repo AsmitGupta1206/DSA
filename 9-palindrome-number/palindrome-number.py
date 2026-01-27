2c891e5b17464fdf231edf7d8484f8e046e11df1
Asmit Gupta <asmitgupta1206@gmail.com>
2026-01-27 23:59:03 +0530

    Time: 2 ms (94.85%) | Memory: 12.4 MB (85.91%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,8 +1,5 @@
 class Solution(object):
     def isPalindrome(self, x):
-        x = str(x)
-        if x == x[::-1]:
-            return True
-        else :
-            return False
-    
+       x=str(x)
+       return x==x[::-1]
+        
