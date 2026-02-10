3c0b3233a3f25794216e4683e9c035967b63ab21
Asmit Gupta <asmitgupta1206@gmail.com>
2026-02-10 21:58:25 +0530

    Time: 4 ms (46.33%) | Memory: 12.6 MB (39.16%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        pairs = {')': '(', '}': '{', ']': '['}
+        
+        for ch in s:
+            if ch in pairs.values():   # opening bracket
+                stack.append(ch)
+            else:  # closing bracket
+                if not stack or stack[-1] != pairs[ch]:
+                    return False
+                stack.pop()
+        
+        return stack == []
+
+        
