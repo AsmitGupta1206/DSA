9cf3e78528545997aafb91596cf793b67a4b5c4b
Asmit Gupta <asmitgupta1206@gmail.com>
2026-08-01 23:58:44 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (5.44%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.java
@@ -0,0 +1,23 @@
+class Solution {
+    Integer[][] dp;
+
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        dp = new Integer[n][n];
+
+        return solve(0, n - 1, nums) >= 0;
+    }
+
+    private int solve(int i, int j, int[] nums) {
+        if (i == j)
+            return nums[i];
+
+        if (dp[i][j] != null)
+            return dp[i][j];
+
+        int takeLeft = nums[i] - solve(i + 1, j, nums);
+        int takeRight = nums[j] - solve(i, j - 1, nums);
+
+        return dp[i][j] = Math.max(takeLeft, takeRight);
+    }
+}
